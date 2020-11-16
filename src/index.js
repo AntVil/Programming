53d3f144fa44890c1c11408de1b0b2d0e1e7b13d
Jandorid <73158569+Jandorid@users.noreply.github.com>
2020-11-16 16:14:49 +0100

    implemented functions
    implemented: customVariableChanger, typeWriter, sleep, and updated index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const dayHandlerModule = require("./DayHandler");
 const inputHandlerModule = require("./InputHandler");
 const constantsModule = require("./constants");
 
-class Main{
-    constructor(){
+class Main {
+    constructor() {
         this.dayHandler = new dayHandlerModule.DayHandler();
         this.inputHandler = new inputHandlerModule.InputHandler();
 
@@ -12,76 +12,70 @@ class Main{
         };
     }
 
-    run(){
+    run() {
         console.log("running program");
         console.log("loading day");
         this.dayHandler.loadNextDay();
-        while(!this.dayHandler.dayIsDone()){
+        while (!this.dayHandler.dayIsDone()) {
             this.runStep();
         }
         this.dayHandler.loadNextDay();
-        while(!this.dayHandler.dayIsDone()){
+        while (!this.dayHandler.dayIsDone()) {
             this.runStep();
         }
     }
 
-    runStep(){
+    runStep() {
         let event = this.dayHandler.popEvent();
-        if(event.getType() === constantsModule.TYPE_STORY){
+        if (event.getType() === constantsModule.TYPE_STORY) {
             this.handleStory(event);
-        }else if(event.getType() === constantsModule.TYPE_INTERACTION){
+        } else if (event.getType() === constantsModule.TYPE_INTERACTION) {
             this.handleInteraction(event);
-        }else if(event.getType() === constantsModule.TYPE_TEXT_INPUT){
+        } else if (event.getType() === constantsModule.TYPE_TEXT_INPUT) {
             this.handleTextInput(event);
-        }else{
+        } else {
             throw new Error(`Error: Unexpected event: '${eventType}'`);
         }
     }
 
-    handleStory(story){
-        if(this.restrictionSatisfied(story.getRestriction())){
-            let storyText = story.getText() + "\n";
-            while (storyText.includes("~N") === true) {
-                storyText = storyText.replace("~N", this.variableDictionary.N);
-            }
-            console.log(storyText);
+    handleStory(story) {
+        if (this.restrictionSatisfied(story.getRestriction())) {
+
+             this.typeWriter(this.customVariableChanger(story.getText()));
 
             let consequences = story.getConsequences();
-            for(let i=0;i<consequences.length;i++){
+            for (let i = 0; i < consequences.length; i++) {
                 this.handleConsequence(consequences[i]);
             }
         }
     }
 
-    handleInteraction(interaction){
-        if(this.restrictionSatisfied(interaction.getRestriction())){
-            let interactionText = interaction.getText() + "\n";
-            while (interactionText.includes("~N") === true) {
-                interactionText = interactionText.replace("~N", this.variableDictionary.N);
-            }
-            console.log(interactionText);
+    handleInteraction(interaction) {
+        if (this.restrictionSatisfied(interaction.getRestriction())) {
+
+            console.log(this.customVariableChanger(interaction.getText()));
 
             let options = interaction.getOptions();
             let possibleOptions = [];
-            for(let i=0;i<options.length;i++){
-                if(this.restrictionSatisfied(options[i].getRestriction())){
+            for (let i = 0; i < options.length; i++) {
+                if (this.restrictionSatisfied(options[i].getRestriction())) {
                     possibleOptions.push(options[i]);
                 }
             }
-            for(let i=0;i<options.length;i++){
+            for (let i = 0; i < options.length; i++) {
                 console.log(`[${i}] ${options[i].getText()}\n`);
             }
 
             let takenOption = this.inputHandler.getOptionInput(options.length);
-            
+
             let consequences = options[takenOption].getConsequences();
-            for(let i=0;i<consequences.length;i++){
+            for (let i = 0; i < consequences.length; i++) {
                 this.handleConsequence(consequences[i]);
             }
         }
     }
 
-    handleTextInput(textInput){
+    handleTextInput(textInput) {
         console.log(textInput.getText());
         this.variableDictionary[textInput.getVariableName()] = this.inputHandler.getTextInput();
         console.log("");
@@ -89,22 +83,44 @@ class Main{
     }
 
 
-    handleConsequence(consequence){
+    handleConsequence(consequence) {
         this.variableDictionary[consequence.getVariableName()] = consequence.getVariableValue();
     }
 
 
-    restrictionSatisfied(restriction){
+    restrictionSatisfied(restriction) {
         let variables = restriction.getVariables();
-        for(let i=0;i<variables.length;i++){
+        for (let i = 0; i < variables.length; i++) {
             let variable = variables[i];
-            if(this.variableDictionary[variable.getVariableName()] !== variable.getVariableValue()){
+            if (this.variableDictionary[variable.getVariableName()] !== variable.getVariableValue()) {
                 return false;
             }
         }
 
         return true;
     }
+    customVariableChanger(input) {
+        let changer = input +  "\n";
+        while (changer.includes("~N") === true) {
+            changer = changer.replace("~N", this.variableDictionary.N);
+        }
+        return changer;
+    }
+    typeWriter(input) {
+        let index = 0 ;
+        while(index < input.length){
+            process.stdout.write(input.charAt(index))
+            this.sleep(50);
+            ++index;
+        }
+    }
+    sleep(milliseconds) {
+        const date = Date.now();
+        let currentDate = null;
+        do {
+          currentDate = Date.now();
+        } while (currentDate - date < milliseconds);
+      }
 }
 
 let main = new Main();
